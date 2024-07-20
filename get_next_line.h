5eb1a3b7f112a7e275e49c943dfcbb567c2e1b9c
Kenny Siwawa <kchikwam@student.42wolfsburg.de>
2024-07-20 10:03:23 +0000

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchikwam <kchikwam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 09:29:52 by kchikwam          #+#    #+#             */
+/*   Updated: 2024/07/20 09:40:38 by kchikwam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
